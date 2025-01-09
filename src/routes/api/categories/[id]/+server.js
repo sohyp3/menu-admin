da4a3d5a3faa7307f4cc2f3197b2f34a910d61e3
sohyp3 <0xshb@protonmail.com>
2025-01-09 20:17:23 +0300

    a shit ton

--- src/routes/api/categories/[id]/+server.js
+++ src/routes/api/categories/[id]/+server.js
@@ -1,4 +1,5 @@
 import { connectDB } from '$lib/helpers/db.js';
+import { ObjectId } from 'mongodb';
 
 export async function GET({ params }) {
     const db = await connectDB();
@@ -17,7 +18,98 @@ export async function GET({ params }) {
 
 export async function POST({ request, params }) {
     const db = await connectDB();
-    const categoryId = params.id; // Parent category ID, if applicable
+    const itemId= params.id; 
+
+    try {
+        const data = await request.json();
+
+        // Validate input (add additional checks as needed)
+        // if (!data.name || !data.name.en || !data.name.tr) {
+        //     return new Response(JSON.stringify({ error: 'Name (en, tr) is required' }), { status: 400 });
+        // }
+
+        if (data.image && !data.image.startsWith('http')) {
+            return new Response(JSON.stringify({ error: 'Invalid image URL' }), { status: 400 });
+        }
+
+        // Generate auto-incremented _id
+
+        const category = {
+            "name": {
+                "en": data.en,
+                "tr": data.tr,
+            },
+            "desc":{
+                "en": data.desc_en,
+                "tr": data.desc_tr,
+            },
+            price: data.price,
+            parent_category_id: categoryId ? categoryId.toString() : null,
+            image: data.image || null,
+            created_at: new Date(),
+        };
+
+        const result = await db.collection('categories').insertOne(category);
+
+        if (!result.acknowledged) {
+            return new Response(JSON.stringify({ error: 'Category not created' }), { status: 500 });
+        }
+
+        return new Response(JSON.stringify(category), { status: 201 });
+    } catch (error) {
+        console.error('Error creating category:', error);
+        return new Response(JSON.stringify({ error: 'Internal server error' }), { status: 500 });
+    }
+}
+
+
+export async function PUT({ request, params }) {
+    const db = await connectDB();
+    const itemId = params.id;
+
+    try {
+        const data = await request.json();
+        const result = await db.collection('items').updateOne(
+            { _id: itemId },
+            { $set: data},
+        );
+
+        if (!result.acknowledged) {
+            return new Response(JSON.stringify({ error: 'item not updated' }), { status: 500 });
+        }
+
+        return new Response(JSON.stringify({ message: 'item updated successfully' }), { status: 200 });
+    } catch (error) {
+        console.error('Error updating item:', error);
+        return new Response(JSON.stringify({ error: 'Internal server error' }), { status: 500 });
+    }
+}
+
+
+export async function DELETE({ params }) {
+    const db = await connectDB();
+    const itemId= params.id;
+
+    try {
+        const result = await db.collection('items').deleteOne({ _id: new ObjectId(itemId) });
+
+        if (!result.acknowledged) {
+            return new Response(JSON.stringify({ error: 'Category not deleted' }), { status: 500 });
+        }
+
+        return new Response(JSON.stringify({ message: 'Category deleted successfully' }), { status: 200 });
+    } catch (error) {
+        console.error('Error deleting category:', error);
+        return new Response(JSON.stringify({ error: 'Internal server error' }), { status: 500 });
+    }
+}
+
+
+
+
+export async function POST({ request, params }) {
+    const db = await connectDB();
+    const categoryId = params.id; 
 
     try {
         const data = await request.json();
--- src/routes/categories/[id]/+page.svelte
+++ src/routes/categories/[id]/+page.svelte
@@ -6,31 +6,43 @@
 	let category_id = $state(null);
 	let all_items= $state([]);
 	let isLoading = $state(true);
-	let editingCategory = $state(null);
+	let editingItem= $state(null);
 
 	async function fetchCategoryData() {
 		try {
-			const subCategories = await fetch(`/api/categories/${category_id}`);
-			if (!subCategories.ok) throw new Error('Failed to fetch parent categories');
-			all_sub_categories = await subCategories.json();
+			const items = await fetch(`/api/categories/${category_id}`);
+			if (!items.ok) throw new Error('Failed to fetch items');
+			all_items= await items.json();
 		} catch (error) {
 			console.error(error);
 		}
 		isLoading = false;
 	}
 
-	async function saveCategory(category) {
+	async function saveItem(item) {
 		try {
-			const response = await fetch(`/api/parent_categories/${category._id}`, {
+			const response = await fetch(`/api/categories/${item._id}`, {
 				method: 'PUT',
 				headers: {
-					'Content-Type': 'application/json',
+					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify(category),
+				body: JSON.stringify(item)
 			});
 			if (!response.ok) throw new Error('Failed to save category');
-			editingCategory = null;
+			editingItem= null;
 			await fetchCategoryData();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async function deleteItem(itemId) {
+		try {
+			const response = await fetch(`/api/categories/${itemId}`, {
+				method: 'DELETE'
+			});
+			if (!response.ok) throw new Error('Failed to delete category');
+			all_items= all_items.filter((item) => item._id !== itemId);
 		} catch (error) {
 			console.error(error);
 		}
@@ -49,143 +61,193 @@
 	});
 </script>
 
-<div class="p-6 min-h-screen bg-gray-50">
-	<h1 class="py-6 text-2xl font-bold text-center text-gray-800 border-b border-gray-300">
-		Parent Categories
-	</h1>
+<div class="flex justify-center bg-gray-50">
+	<div class="  w-[960px]">
+		<h1 class="py-6 text-2xl font-bold text-center text-gray-800 border-b border-gray-300">
+			Categories
+		</h1>
 
-	{#if isLoading}
-		<p class="mt-6 text-center text-gray-600">Loading...</p>
-	{:else}
-		<div class="grid gap-6 mt-6">
-			{#each all_sub_categories as category (category._id)}
-				<div class="block p-4 bg-white rounded-lg border border-gray-200 shadow-sm">
-					{#if editingCategory === category._id}
-						<div>
-							<label class="block text-sm font-medium text-gray-600">English Name</label>
-							<input
-								type="text"
-								class="p-2 w-full rounded-lg border border-gray-300 focus:ring focus:ring-blue-300 focus:outline-none"
-								bind:value={category.name['en']}
-							/>
+		{#if isLoading}
+			<p class="mt-6 text-center text-gray-600">Loading...</p>
+		{:else}
+			<div class="grid gap-6 mt-6">
+				{#each all_items as item (item._id)}
+					<div class="block p-4 bg-white rounded-lg border border-gray-200 shadow-sm">
+						{#if editingItem=== item._id}
+							<div>
+								<label class="block text-sm font-medium text-gray-600">English Name</label>
+								<input
+									type="text"
+									class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+									bind:value={item.name['en']}
+								/>
 
-							<label class="block mt-2 text-sm font-medium text-gray-600">Turkish Name</label>
-							<input
-								type="text"
-								class="p-2 w-full rounded-lg border border-gray-300 focus:ring focus:ring-blue-300 focus:outline-none"
-								bind:value={category.name['tr']}
-							/>
+								<label class="block mt-2 text-sm font-medium text-gray-600">Turkish Name</label>
+								<input
+									type="text"
+									class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+									bind:value={item.name['tr']}
+								/>
 
-							<div class="flex gap-3 mt-4">
-								<button
-									on:click={() => saveCategory(category)}
-									class="px-4 py-2 font-medium text-white bg-green-600 rounded-lg transition hover:bg-green-700"
-								>
-									Save
-								</button>
-								<button
-									on:click={() => (editingCategory = null)}
-									class="px-4 py-2 font-medium text-white bg-gray-400 rounded-lg transition hover:bg-gray-500"
-								>
-									Cancel
-								</button>
+								<label class="block mt-2 text-sm font-medium text-gray-600">Image URL</label>
+								<input
+									type="text"
+									class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+									bind:value={item.image}
+								/>
+
+								<div class="flex gap-3 mt-4">
+									<button
+										on:click={() => saveItem(item)}
+										class="px-4 py-2 font-medium text-white bg-green-600 rounded-lg transition hover:bg-green-700"
+									>
+										Save
+									</button>
+									<button
+										on:click={() => (editingItem= null)}
+										class="px-4 py-2 font-medium text-white bg-gray-400 rounded-lg transition hover:bg-gray-500"
+									>
+										Cancel
+									</button>
+								</div>
 							</div>
-						</div>
-					{:else}
-						<div class="text-lg font-medium text-gray-700">
-							<h1 class="mb-2">Name:</h1>
-							<p class="text-gray-900">En: {category.name['en']}</p>
-							<p class="text-gray-900">Tr: {category.name['tr']}</p>
-							<button
-								on:click={() => (editingCategory = category._id)}
-								class="px-4 py-2 mt-4 font-medium text-white bg-blue-600 rounded-lg transition hover:bg-blue-700"
-							>
-								Edit
-							</button>
-						</div>
-					{/if}
-				</div>
-			{/each}
-		</div>
-	{/if}
+						{:else}
+							<div class="text-lg font-medium text-gray-700">
+								<h1 class="mb-2">Name:</h1>
+								<p class="text-gray-900">En: {item.name['en']}</p>
+								<p class="text-gray-900">Tr: {item.name['tr']}</p>
+								<p class="text-gray-900">
+									Image: <a
+										href={item.image}
+										target="_blank"
+										class="text-blue-500 hover:underline">View Image</a
+									>
+								</p>
+								<div class="flex gap-3 mt-4">
+									<button
+										on:click={() => (editingItem= item._id)}
+										class="px-4 py-2 font-medium text-white bg-blue-600 rounded-lg transition hover:bg-blue-700"
+									>
+										Edit
+									</button>
+									<button
+										on:click={() => deleteItem(item._id)}
+										class="px-4 py-2 font-medium text-white bg-red-600 rounded-lg transition hover:bg-red-700"
+									>
+										Delete
+									</button>
+								</div>
+							</div>
+						{/if}
+					</div>
+				{/each}
+			</div>
+		{/if}
 
-	<form
-		class="p-6 mt-10 space-y-4 bg-white rounded-lg border border-gray-200 shadow-sm"
-		on:submit|preventDefault={async (event) => {
-			const formData = new FormData(event.target);
-			const jsonData = Object.fromEntries(formData.entries());
-			const response = await fetch(`/api/parent_categories/${parent_category_id}`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(jsonData),
-			});
-			if (!response.ok) {
-				console.error('Failed to create new parent category');
-			} else {
-				const newCategory = await response.json();
-				all_sub_categories = [...all_sub_categories, newCategory];
-			}
-		}}
-	>
-		<h2 class="text-xl font-semibold text-gray-800">Create New Parent Category</h2>
-
-		<div>
-			<label for="name-en" class="block text-sm font-medium text-gray-600">
-				English Name
-			</label>
-			<input
-				type="text"
-				id="name-en"
-				name="en"
-				class="p-2 w-full rounded-lg border border-gray-300 focus:ring focus:ring-blue-300 focus:outline-none"
-				required
-			/>
-		</div>
-
-		<div>
-			<label for="name-tr" class="block text-sm font-medium text-gray-600">
-				Turkish Name
-			</label>
-			<input
-				type="text"
-				id="name-tr"
-				name="tr"
-				class="p-2 w-full rounded-lg border border-gray-300 focus:ring focus:ring-blue-300 focus:outline-none"
-				required
-			/>
-		</div>
-
-		<div>
-			<label for="parent-category-id" class="block text-sm font-medium text-gray-600">
-				Parent Category ID
-			</label>
-			<input
-				type="text"
-				id="parent-category-id"
-				name="parent_category_id"
-				class="p-2 w-full rounded-lg border border-gray-300 focus:ring focus:ring-blue-300 focus:outline-none"
-			/>
-		</div>
-
-		<div>
-			<label for="image" class="block text-sm font-medium text-gray-600">
-				Image URL
-			</label>
-			<input
-				type="text"
-				id="image"
-				name="image"
-				class="p-2 w-full rounded-lg border border-gray-300 focus:ring focus:ring-blue-300 focus:outline-none"
-			/>
-		</div>
-
-		<button
-			type="submit"
-			class="px-4 py-2 w-full font-medium text-white bg-blue-600 rounded-lg transition hover:bg-blue-700"
+		<form
+			class="p-6 mt-10 space-y-4 bg-white rounded-lg border border-gray-200 shadow-sm"
+			on:submit|preventDefault={async (event) => {
+				const formData = new FormData(event.target);
+				const jsonData = Object.fromEntries(formData.entries());
+				const response = await fetch(`/api/categories/${category_id}`, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(jsonData)
+				});
+				if (!response.ok) {
+					console.error('Failed to create new parent category');
+				} else {
+					const newItem= await response.json();
+					all_items= [...all_items, newItem];
+				}
+			}}
 		>
-			Create New Parent Category
-		</button>
-	</form>
+			<h2 class="text-xl font-semibold text-gray-800">Create New Parent Category</h2>
+
+			<div>
+				<label for="name-en" class="block text-sm font-medium text-gray-600"> English Name </label>
+				<input
+					type="text"
+					id="name-en"
+					name="en"
+					class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+					required
+				/>
+			</div>
+
+			<div>
+				<label for="name-tr" class="block text-sm font-medium text-gray-600"> Turkish Name </label>
+				<input
+					type="text"
+					id="name-tr"
+					name="tr"
+					class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+					required
+				/>
+			</div>
+
+			<div>
+				<label for="desc-en" class="block text-sm font-medium text-gray-600"> English Description</label>
+				<input
+					type="text"
+					id="desc-en"
+					name="desc_en"
+					class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+					required
+				/>
+			</div>
+
+			<div>
+				<label for="desc-tr" class="block text-sm font-medium text-gray-600"> Turkish Description</label>
+				<input
+					type="text"
+					id="desc-tr"
+					name="desc_tr"
+					class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+					required
+				/>
+			</div>
+
+			<div>
+				<label for="parent-category-id" class="block text-sm font-medium text-gray-600">
+					Parent Category ID
+				</label>
+				<input
+					type="text"
+					id="parent-category-id"
+					name="parent_category_id"
+					class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+				/>
+			</div>
+
+
+			<div>
+				<label for="price" class="block text-sm font-medium text-gray-600"> Price</label>
+				<input
+					type="text"
+					id="price"
+					name="price"
+					class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+				/>
+			</div>
+			<div>
+				<label for="image" class="block text-sm font-medium text-gray-600"> Image URL </label>
+				<input
+					type="text"
+					id="image"
+					name="image"
+					class="p-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300"
+				/>
+			</div>
+
+			<button
+				type="submit"
+				class="px-4 py-2 w-full font-medium text-white bg-blue-600 rounded-lg transition hover:bg-blue-700"
+			>
+				Create New Item
+			</button>
+		</form>
+	</div>
 </div>
