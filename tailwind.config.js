b9829463df7e70598b65e84ced4ca5132e2f929d
sohyp3 <0xshb@protonmail.com>
2025-01-09 12:02:18 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [forms]
+};
