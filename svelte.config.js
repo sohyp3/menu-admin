b995c70e3c0f0b1aecf2c3e0e0f579fdf4881497
sohyp3 <0xshb@protonmail.com>
2025-01-15 15:59:25 +0300

    disabled jsdoc thank god

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -7,7 +8,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
 };
 
 export default config;
